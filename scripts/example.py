fa3dbc66455af1b22edff87b024415b2e6ae73b4
VasilyevEvgeny <vasilev.evgeniy@physics.msu.ru>
2020-01-05 22:48:35 +0300

    spectra examples

--- scripts/example.py
+++ scripts/example.py
@@ -1,7 +1,7 @@
 from core import ProcessorFAS, ProcessorIFS
 
-gauss_fas = ProcessorFAS('scripts/example_data/gauss_fas', log_scale=True)
-gauss_fs = ProcessorIFS('scripts/example_data/gauss_ifs', log_scale=True)
+gauss_fas = ProcessorFAS('scripts/example_data/gauss_fas', steps_overlap=5)
+gauss_fs = ProcessorIFS('scripts/example_data/gauss_ifs')
 
-vortex_fas = ProcessorFAS('scripts/example_data/vortex_fas', log_scale=True)
-vortex_fs = ProcessorIFS('scripts/example_data/vortex_ifs', log_scale=True)
+vortex_fas = ProcessorFAS('scripts/example_data/vortex_fas')
+vortex_fs = ProcessorIFS('scripts/example_data/vortex_ifs')
