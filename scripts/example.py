14d0eb790fb2387162e46633718371821c82ac6c
VasilyevEvgeny <vasilev.evgeniy@physics.msu.ru>
2020-01-05 22:57:24 +0300

    fas gauss and vortex

--- scripts/example.py
+++ scripts/example.py
@@ -1,6 +1,6 @@
 from core import ProcessorFAS, ProcessorIFS
 
-gauss_fas = ProcessorFAS('scripts/example_data/gauss_fas', steps_overlap=5)
+gauss_fas = ProcessorFAS('scripts/example_data/gauss_fas')
 gauss_fs = ProcessorIFS('scripts/example_data/gauss_ifs')
 
 vortex_fas = ProcessorFAS('scripts/example_data/vortex_fas')
